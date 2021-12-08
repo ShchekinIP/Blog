d8f4f1c6ae5cf111ad60438a8cd39ad4de1aa43a
ShchekinIP <63049086+ShchekinIP@users.noreply.github.com>
2021-12-08 18:36:55 +0400

    Add add post functional

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment:Environment= {
   production: true,
-  apiKey: 'AIzaSyB0JpAuUvPJF44bdBOFGBOi2KGKrSyxgCc'
+  apiKey: 'AIzaSyB0JpAuUvPJF44bdBOFGBOi2KGKrSyxgCc',
+  fbDbUrl: 'https://angular-blog-496f3-default-rtdb.firebaseio.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment : Environment = {
   production: false,
-  apiKey: 'AIzaSyB0JpAuUvPJF44bdBOFGBOi2KGKrSyxgCc'
+  apiKey: 'AIzaSyB0JpAuUvPJF44bdBOFGBOi2KGKrSyxgCc',
+  fbDbUrl: 'https://angular-blog-496f3-default-rtdb.firebaseio.com/'
 };
 
 /*
