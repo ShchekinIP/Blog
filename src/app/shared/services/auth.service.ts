b956dbb16f1abdcea52cf673e58ec5acd1b37afe
ShchekinIP <63049086+ShchekinIP@users.noreply.github.com>
2021-12-08 15:41:18 +0400

    add auth firebase token and rout guard

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,74 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {FbAuthResponse, User} from "../../admin/shared/interfaces";
+import {Observable, Subject, throwError} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {catchError, tap} from "rxjs/operators";
+
+@Injectable()
+export class AuthService{
+
+  public error$ : Subject<string> = new Subject<string>()
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  login(user : User) : Observable<any>{
+    user.returnSecureToken = true
+
+
+
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(tap(this.setToken), catchError(this.handleError.bind(this)))
+  }
+
+  logout(){
+    this.setToken(null)
+
+  }
+
+  private handleError(error:HttpErrorResponse){
+    const {message} = error.error.error
+    switch (message){
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный email')
+        break
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный пароль')
+        break
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Email не существует')
+        break
+    }
+    return throwError(error)
+
+  }
+
+  isAuthenticated() : boolean {
+    return !!this.token
+  }
+
+  get token():string {
+    const expDate = new Date(localStorage.getItem('fb-token-exp'))
+    if (new Date() > expDate){
+      this.logout()
+      return null
+    }
+    return localStorage.getItem('fb-token')
+  }
+
+  private setToken(response: any|null){
+    if (response){
+      const expDate = new Date(new Date().getTime()+ +response.expiresIn * 1000)
+      localStorage.setItem('fb-token', response.idToken)
+      localStorage.setItem('fb-token-exp', expDate.toString())
+    }
+    else {
+      localStorage.clear()
+    }
+
+
+  }
+}
