72c3b3785739800edad2e7afcb0c169bbdaa7b87
ShchekinIP <63049086+ShchekinIP@users.noreply.github.com>
2021-12-16 13:13:04 +0400

    Add dynamic post view

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostService} from "../admin/shared/post.service";
+import {Observable} from "rxjs";
+import {Post} from "../admin/shared/interfaces";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  post$ : Observable<Post>
+
+  constructor(
+    private route:ActivatedRoute,
+    private postServise:PostService
+  ) { }
 
   ngOnInit() {
+    this.post$ = this.route.params.pipe(switchMap((params:Params) => {
+      return this.postServise.getById(params['id'])
+    }))
   }
 
 }
